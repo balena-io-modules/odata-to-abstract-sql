e9f636b894c00b1b29946ddcb0f7bb635cb0d01d
Andrea Rosci <jsreds@Andreas-MBP.homenet.telecomitalia.it>
2024-10-03 13:45:51 +0200

    Fix OrderBy to use LEFT JOIN instead of INNER JOIN
    Change-type: patch

--- test/chai-sql.js
+++ test/chai-sql.js
@@ -48,6 +48,17 @@ chai.use(function ($chai, utils) {
 			);
 			return this;
 		};
+	const binaryClause = (bodyType) =>
+		function (...bodyClauses) {
+			const obj = utils.flag(this, 'object');
+			for (let i = 0; i < bodyClauses.length; i++) {
+				expect(obj).to.contain.something.that.deep.equals(
+					[bodyType, bodyClauses[i][0], bodyClauses[i][1]],
+					bodyType + ' - ' + i,
+				);
+			}
+			return this;
+		};
 
 	const select = (function () {
 		const bodySelect = bodyClause('Select');
@@ -76,6 +87,15 @@ chai.use(function ($chai, utils) {
 			return ['Alias', ['Table', v[0]], v[1]];
 		});
 		return fromClause.apply(this, bodyClauses);
+	});
+	const leftJoinClause = binaryClause('LeftJoin');
+	utils.addMethod(assertionPrototype, 'leftJoin', function (...bodyClauses) {
+		bodyClauses = bodyClauses.map(function ([v, condition]) {
+			const resource =
+				typeof v === 'string' ? ['Table', v] : ['Alias', ['Table', v[0]], v[1]];
+			return [resource, ['On', condition]];
+		});
+		return leftJoinClause.apply(this, bodyClauses);
 	});
 	utils.addMethod(assertionPrototype, 'where', bodyClause('Where'));
 	utils.addMethod(assertionPrototype, 'orderby', function (...bodyClauses) {
